461a6ff9ef1f5817e654b8eef8dcc43c81b3e588
759140 <tirumalasaiteja988@gmail.com>
2022-04-02 23:38:17 +0530

    initial commit

---
+++ orders.js
@@ -0,0 +1,44 @@
+const Orders = [
+  {
+    productName: "Foldable Mini Drone",
+    productNumber: "85631",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "Foldable Mini Drone1",
+    productNumber: "85631",
+    paymentStatus: "Due",
+    shipping: "Declined",
+  },
+  {
+    productName: "Foldable Mini Drone2",
+    productNumber: "85631",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "Foldable Mini Drone3",
+    productNumber: "8590",
+    paymentStatus: "paid",
+    shipping: "Delivered",
+  },
+  {
+    productName: "Foldable Mini Drone4",
+    productNumber: "85631",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "Foldable Mini Drone5",
+    productNumber: "85631",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "Foldable Mini Drone6",
+    productNumber: "85631",
+    paymentStatus: "Paid",
+    shipping: "Delivered",
+  },
+];
